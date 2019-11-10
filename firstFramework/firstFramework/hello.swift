4cfe1a9032a8674c0a0bf23c5a57e4af529ba422
Ermilov Arseniy <b9125@yandex.ru>
2019-11-10 20:28:32 +0300

    framework

---
+++ firstFramework/firstFramework/hello.swift
@@ -0,0 +1,15 @@
+//
+//  hello.swift
+//  firstFramework
+//
+//  Created by Арсений Ермилов   on 10/11/2019.
+//  Copyright © 2019 ArsIndustries. All rights reserved.
+//
+
+import UIKit
+
+class hello: NSObject {
+    func sayHi(){
+        print("hi")
+    }
+}
