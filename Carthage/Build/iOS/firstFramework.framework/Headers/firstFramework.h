95a25312a4018049eb74242c072cf7172f026c78
Arseniy <b9125@yandex.ru>
2019-11-10 21:42:43 +0300

    first commit

---
+++ Carthage/Build/iOS/firstFramework.framework/Headers/firstFramework.h
@@ -0,0 +1,19 @@
+//
+//  firstFramework.h
+//  firstFramework
+//
+//  Created by Арсений Ермилов   on 10/11/2019.
+//  Copyright © 2019 ArsIndustries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for firstFramework.
+FOUNDATION_EXPORT double firstFrameworkVersionNumber;
+
+//! Project version string for firstFramework.
+FOUNDATION_EXPORT const unsigned char firstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <firstFramework/PublicHeader.h>
+
+
---
+++ firstFramework/abra.swift
@@ -0,0 +1,15 @@
+//
+//  abra.swift
+//  firstFramework
+//
+//  Created by Арсений Ермилов   on 10/11/2019.
+//  Copyright © 2019 ArsIndustries. All rights reserved.
+//
+
+import UIKit
+
+open class abra: NSObject {
+    public func sayHi(){
+        print("hi")
+    }
+}
